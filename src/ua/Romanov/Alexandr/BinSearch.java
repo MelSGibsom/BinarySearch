98bdcdcfd1d97f39a2582453015fa12c42aa9d3b
Alexandr Romanov <komphelp23@gmail.com>
2019-12-15 03:12:25 +0200

    first

---
+++ src/ua/Romanov/Alexandr/BinSearch.java
@@ -0,0 +1,141 @@
+package ua.Romanov.Alexandr;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinSearch {
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    static void merge(int arr[], int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    static void sort(int arr[], int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = (l + r) / 2;
+
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+    public static int binarySearch(int arr[], int elementToSearch) {
+
+        int firstIndex = 0;
+        int lastIndex = arr.length - 1;
+
+        // условие прекращения (элемент не представлен)
+        while (firstIndex <= lastIndex) {
+            int middleIndex = (firstIndex + lastIndex) / 2;
+            // если средний элемент - целевой элемент, вернуть его индекс
+            if (arr[middleIndex] == elementToSearch) {
+                return middleIndex;
+            }
+
+            // если средний элемент меньше
+            // направляем наш индекс в middle+1, убирая первую часть из рассмотрения
+            else if (arr[middleIndex] < elementToSearch)
+                firstIndex = middleIndex + 1;
+
+                // если средний элемент больше
+                // направляем наш индекс в middle-1, убирая вторую часть из рассмотрения
+            else if (arr[middleIndex] > elementToSearch)
+                lastIndex = middleIndex - 1;
+
+        }
+        return -1;
+    }
+
+    /* A utility function to print array of size n */
+    static void printArray(int arr[]) {
+        //System.out.print("Исходный массив: ");
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void print(int elementToSearch, int index) {
+        if (index == -1) {
+            System.out.println(elementToSearch + " not found.");
+        } else {
+            System.out.println(elementToSearch + " found at index: " + index);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 90, -7, 58, 78, 2, 34, 56, -56};
+        System.out.print("Исходный массив: ");
+        printArray(arr);
+        System.out.println("чтобы искать число, необходимо сначала отсортировать массив по порядку");
+
+        sort(arr, 0, arr.length - 1);
+        System.out.print("Отсортированный массив: ");
+        printArray(arr);
+        System.out.print("Введите число: ");
+        for (; ; ) {
+            Scanner scanner = new Scanner(System.in);
+            int elementToSearch = scanner.nextInt();
+            int index = binarySearch(arr, elementToSearch);
+            print(elementToSearch, index);
+
+
+        }
+
+
+    }
+}
